154e82ab832715dde53cb7a35886907aaf0fdaa1
Matthias Colin <matthias.colin@gmail.com>
2020-07-16 10:26:13 +0200

    wordcount proto

---
+++ PremiersPas/wordcount.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	mots := []string{"go", "python", "java", "go", "ruby", "php", "r", "go", "c#", "python"}
+	fmt.Printf("%d %v\n", len(mots), mots)
+	var res map[string]uint
+
+	fmt.Printf("%d %v\n", len(res), res)
+}
